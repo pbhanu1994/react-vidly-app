acfb5de405fd570f54b5d714b5ce185ae876e181
Bhanu <pbhanu.1994@gmail.com>
2019-02-11 11:30:17 +0530

    Login form completed

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,89 +1,41 @@
-import React, { Component } from "react";
-import Input from "./common/inputForm";
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
-class Login extends Component {
+class Login extends Form {
   state = {
-    account: { username: "", password: "" },
-    errors: {
-      username: ""
-    }
-  };
-
-  validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      // return value.trim() === "" && "Username is required";
-      if (value.trim() === "") return "Username is required";
-    }
-    if (name === "password") {
-      // return value.trim() === "" && "Password is required";
-      if (value.trim() === "") return "Password is required";
-    }
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account, errors });
+    data: { username: "", password: "" },
+    errors: {}
   };
 
   //Creating Ref for form elements
-  username = React.createRef();
+  // username = React.createRef();
 
   //   componentDidMount() {
   //     this.username.current.focus();
   //   }
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
 
-  validate() {
-    const errors = {};
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required";
-    if (account.password.trim() === "")
-      errors.password = "Password is required";
-
-    return Object.keys(errors).length === 0 ? null : errors;
-  }
-
-  handleSubmit = e => {
-    //Prevents the Submission of the form (which causes the full page reload)
-    e.preventDefault();
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
+  doSubmit = () => {
     //Call the Server
     console.log("Submitted");
-    //Getting the form values in React
-    // const username = this.username.current.value;
   };
 
   render() {
-    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login Form</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            value={account.username}
-            label="Username"
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-          <Input
-            name="password"
-            value={account.password}
-            label="Password"
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-          <button className="btn-primary btn-lg">Login</button>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
